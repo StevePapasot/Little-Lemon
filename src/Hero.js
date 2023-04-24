b009fa82efca0fd450ee11bdd72a2d6e54c4e9e2
StevenPapasot <75527414+StevePapasot@users.noreply.github.com>
2023-04-24 14:33:35 +0300

    Hero section with media

--- src/Hero.js
+++ src/Hero.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Hero.css";
+import First from "./First.png";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -9,14 +10,26 @@ function Hero() {
     <section className="hero">
       <div className="container">
         <div className="row">
-          <div className="col-md-6">
+          <div className="left">
             <h1>Little Lemon</h1>
             <h3>Chicago</h3>
-            <p>Some description text here.</p>
-            <button>Call to Action</button>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.Duis
+              aute irure dolor in reprehenderit in voluptate velit esse cillum
+              dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+              non proident, sunt in culpa qui officia deserunt mollit anim id
+              est laborum.
+            </p>
+            <button>Reserve a Table</button>
           </div>
-          <div className="col-md-6">
-            <img src="https://placehold.it/600x400" alt="Right Column Image" />
+          <div className="right">
+            <img src={First} alt="Right Column Image" />
           </div>
         </div>
       </div>
